4deedd6345369d1a947d5696150334eb74c85554
Jensen Lawless <jensenlawless@gmail.com>
2023-06-16 12:36:09 -0600

    com

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 6, 'M', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Rowdy', 6, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Tabitha', 6, 'M', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Sergeant Brown', 6, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Melissa', 6, 'M', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Grinch', 6, 'M', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Wendy', 6, 'M', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 6, 'M', 'brown', 'calm', 1);
